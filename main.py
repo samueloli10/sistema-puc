3c86aa2d9baecebbe7f9c36fcc18bb0fe575c404
Samuel Oliveira <samueloli2000@gmail.com>
2023-03-17 00:01:30 -0300

    Implementação das Estruturas de repetição

--- main.py
+++ main.py
@@ -1,27 +1,40 @@
 nome_aluno = " "
 codigo_aluno = 0
+opcao = 1
 
-print("-----MENU-----")
-print("1 - INCLUIR")
-print("2 - LISTAR")
-print("3 - ALTERAR")
-print("4 - EXCLUIR")
-print("0 - SAIR")
+while opcao != 0:
+    print("\n\n-----MENU-----")
+    print("1 - INCLUIR")
+    print("2 - LISTAR")
+    print("3 - ALTERAR")
+    print("4 - EXCLUIR")
+    print("0 - SAIR")
 
-opcao = int(input("Digite a opção: "))
-
-if opcao == 1:
-    nome_aluno = input("Digite o nome do aluno: ")
-    codigo_aluno = input("Digite o código do aluno: ")
-elif opcao == 2:
-    print("O nome do aluno é ", nome_aluno)
-    print("O código do alunoe é ", codigo_aluno)
-elif opcao == 3:
-    nome_aluno = input("Digite o novo nome do aluno: ")
-    codigo_aluno = input("Digite o novo código do aluno:")
-elif opcao == 4:
-    nome_aluno = " "
-    codigo_aluno = 0
-else :
-    print("Opção invalida!")
-    
+    while True:
+        try:
+            opcao = int(input("Digite a opção: "))
+            if opcao == 1:
+                nome_aluno = input("\n\nDigite o nome do aluno: ")
+                codigo_aluno = input("Digite o código do aluno: ")
+                print("\n\n")
+            elif opcao == 2:
+                print("\n\nO nome do aluno é ", nome_aluno)
+                print("O código do aluno é ", codigo_aluno, "\n\n")
+            elif opcao == 3:
+                nome_aluno = input("\n\nDigite o novo nome do aluno: ")
+                codigo_aluno = input("Digite o novo código do aluno:")
+                print("\n\n")
+            elif opcao == 4:
+                print("\n\n")
+                nome_aluno = " "
+                codigo_aluno = 0
+                print("\n\n")
+            elif opcao == 0:
+                print("\n\nVocê escolheu sair.\n\n")
+                break
+            else :
+                print("\n\nDigite uma opção válida.")
+                break
+            break
+        except ValueError: 
+            print("\n\nDigitou um valor inválido.\n\n")
