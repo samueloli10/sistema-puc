6a09962e65b24e4dd2f430ab0bea6e682bbb376d
Samuel Oliveira <samueloli2000@gmail.com>
2023-03-18 15:49:09 -0300

    menu principal

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 nome_aluno = " "
 codigo_aluno = 0
-opcao = 1
+opcao = ' '
 
 while opcao != 0:
+
+    print("---- MENU PRINCIPAL ----")
+    print("(1) - Gerenciar estudantes.")
+    print("(2) - Gerenciar professores.")
+    print("(3) - Gerenciar disciplinas.")
+    print("(4) - Gerenciar turmas.")
+    print("(5) - Gerenciar matrículas.")
+
     print("\n\n-----MENU-----")
     print("1 - INCLUIR")
     print("2 - LISTAR")
     print("3 - ALTERAR")
     print("4 - EXCLUIR")
     print("0 - SAIR")
+
     while True:
         try:
             opcao = int(input("Digite a opção: "))
@@ -18,8 +27,8 @@ while opcao != 0:
                 print("\n")
                 break
             elif opcao == 2:
-                print(f'\nNome do aluno: {nome_aluno}')
-                print(f'Código do aluno: {codigo_aluno}\n')
+                print("\nO nome do aluno é ", nome_aluno)
+                print("O código do aluno é ", codigo_aluno, "\n")
                 break
             elif opcao == 3:
                 nome_aluno = input("\nDigite o novo nome do aluno: ")
@@ -27,13 +36,15 @@ while opcao != 0:
                 print("\n")
                 break
             elif opcao == 4:
+                print("\n")
                 nome_aluno = " "
                 codigo_aluno = 0
+                print("\n")
                 break
             elif opcao == 0:
                 print("\n*** Você escolheu sair. ***\n")
                 break
             else :
-                print("\n*** Digite uma opção válida! ***\n")
+                print("\n*** Digite uma opção válida! ***")
         except ValueError: 
             print("\n*** Digitou um valor inválido. ***\n")
