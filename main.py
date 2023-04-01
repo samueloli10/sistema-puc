ca6c2a58f74a8d86a5d12ee4a80e9dbd1eab4053
Samuel Oliveira <samueloli2000@gmail.com>
2023-04-01 00:40:58 -0300

    Criação da função que cria uma lista sem uma determinado dicionário encontrado

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def menu_principal():
             input("\nPressione ENTER para continuar...")
 
 def menu_operacoes(opcao_principal):
-    lista_opcoes = ['Estudantes', 'Professores', 'Disciplinas', 'Turmas', 'Matrículas', 'Sair' ]
+    lista_opcoes = ['ESTUDANTE', 'PROFESSOR', 'DISCIPLINA', 'TURMA', 'MATRÍCULA', 'SAIR1' ]
     while True:
         print(f'\n***** [{lista_opcoes[opcao_principal - 1]}] MENU DE OPERAÇÕES *****')
         print("(1) Incluir.\n(2) Listar.\n(3) Atualizar.\n(4) Excluir.\n(5) Voltar ao menu principal.\n")
@@ -50,14 +50,15 @@ def inicio():
                 opcao_operacao = menu_operacoes(opcao_principal)
                 if opcao_operacao == 1:          
                     try: 
-                        print(f"\n===== [ESTUDANTE] INCLUSÃO =====")  
+                        print(f"\n===== [ESTUDANTE] INCLUIR =====")  
                         codigo = int(input("Digite o código: "))
                         nome = input("Digite o nome: ")
                         cpf = input("Digite o CPF: ")                  
                         if verificar_dados(lista_estudantes, codigo) is None:
                             if verificar_dados(lista_estudantes, cpf) is None:
-                                dicionario = {'Código': codigo, 'Nome': nome, 'CPF': cpf}
-                                create(lista_estudantes, dicionario)
+                                dicionario_encontrado = {'Código': codigo, 'Nome': nome, 'CPF': cpf}
+                                create(lista_estudantes, dicionario_encontrado)
+                                print("\nCadastro realizado com sucesso...")
                             else:
                                 print(f"\n===== ERRO =====")  
                                 print(f'O CPF: {cpf} está presente no cadastro:\n{verificar_dados(lista_estudantes, cpf)}\n\nOperação cancelada...')  
@@ -71,16 +72,51 @@ def inicio():
                         print("\n***** OCORREU UM ERRO. *****\n")
                         input("\nPressione ENTER para continuar...")
                 elif opcao_operacao == 2:
-                    print("\n===== [ESTUDANTE] LISTAGEM =====\n")
-                    read(lista_estudantes)
+                    print("\n===== [ESTUDANTE] LISTAR =====\n")
+                    if read_all(lista_estudantes) == False:
+                        print("\n***** NÃO HÁ ESTUDANTES CADASTRADOS. *****\n")
+                        input("\nPressione ENTER para continuar...")
+                    else:
+                        input("\nPressione ENTER para continuar...")
+                elif opcao_operacao == 3:
+                    try:
+                        print("\n===== [ESTUDANTE] ATUALIZAR =====\n")
+                        codigo = int(input("BUSCAR: Digite o código: "))
+                        dicionario_encontrado = verificar_dados(lista_estudantes, codigo)
+                        if dicionario_encontrado is None:
+                            print("\nCadastro não encontrado...")
+                        else:
+                            print("\nCadastro encontrado...\n")
+                            read_one(dicionario_encontrado)  
+                            codigo = int(input("Digite o novo código: "))
+                            nome = input("Digite o novo nome: ")
+                            cpf = input("Digite o novo CPF: ")
+                            if verificar_dados(lista_sem_repetir(lista_estudantes, dicionario_encontrado), codigo) is None :
+                                if verificar_dados(lista_sem_repetir(lista_estudantes, dicionario_encontrado), cpf) is None:
+                                    dicionario_novo = {'Código': codigo, 'Nome': nome, 'CPF': cpf}
+                                    update(lista_estudantes, dicionario_novo, dicionario_encontrado)
+                                    print("\nCadastro atualizado com sucesso...")
+                                else:
+                                    print(f"\n===== ERRO =====")  
+                                    print(f'O CPF: {cpf} não pode ser utilizado.\n\nOperação cancelada...')  
+                            else:
+                                print(f"\n===== ERRO =====")  
+                                print(f'O Código: {codigo} não pode ser utilizado.\n\nOperação cancelada...')  
+                    except ValueError:
+                        print("\n***** VALOR INVÁLIDO. *****\n")
+                        input("\nPressione ENTER para continuar...")
+                    except:
+                        print("\n***** OCORREU UM ERRO. *****\n")
+                        input("\nPressione ENTER para continuar...")
                 elif opcao_operacao == 4:
                     try: 
                         print("\n===== [ESTUDANTE] EXCLUIR =====\n")
-                        codigo = int(input("Digite o código: "))
+                        codigo = int(input("BUSCAR: Digite o código: "))
                         if verificar_dados(lista_estudantes, codigo) is None:
                             print("\nNão existe cadastro com esse valor...")
                         else:
                             delete(lista_estudantes, verificar_dados(lista_estudantes, codigo))
+                            print("\nCadastro excluído com sucesso...")
                     except ValueError:
                         print("\n***** VALOR INVÁLIDO. *****\n")
                         input("\nPressione ENTER para continuar...")
@@ -107,26 +143,37 @@ def verificar_dados(lista, valor_procurado):
                 return dicionario
     return None
 
+def lista_sem_repetir(lista, dicionario_encontrado):
+    lista_sem_repetidos = [] 
+    for dicionario in lista:
+        if dicionario == dicionario_encontrado:
+            return lista_sem_repetidos
+        lista_sem_repetidos.append(dicionario)
+        return lista_sem_repetidos
+
 def create(lista, dicionario):
     lista.append(dicionario)
-    print("\nCadastro concluído...")
              
-def read(lista):
+def read_all(lista):
     if len(lista) != 0:
         for dicionario in lista:
             for chave, valor in dicionario.items():
                 print(f'{chave}: {valor}')    
-            print("\n==================\n")             
-        input("\nPressione ENTER para continuar...")    
+            print("\n==================\n") 
+        return True              
     else:
-        print("\n***** NÃO HÁ ESTUDANTES CADASTRADOS. *****\n")
-        input("\nPressione ENTER para continuar...")
-        
-def update():
-    return
+        return False
+
+def read_one(dicionario):
+    for chave, valor in dicionario.items():
+        print(f'{chave}: {valor}')   
+    print("\n==================\n") 
+
+def update(lista, dicionario_novo, dicionario_atual):
+    delete(lista, dicionario_atual)
+    create(lista, dicionario_novo)
 
 def delete(lista, dicionario):
     lista.remove(dicionario)
-    print("\nCadastro excluído...")
 
 inicio() #inicio do sistema
